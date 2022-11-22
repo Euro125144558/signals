744a1bf36863eb352704362e81b99842e939a120
Euro125144558 <117720659+Euro125144558@users.noreply.github.com>
2022-11-22 15:50:16 +0700

    Create Insurance.py

---
+++ Insurance.py
@@ -0,0 +1,13 @@
+age = int(input("enter your age: "))
+if age < 25:
+    print("high")
+else:
+    type_car = input("type of car: fast car, family car (fast/fam)")
+    if type _car == "fam":
+        print("low")
+    else:
+        car_accident = input("had an accident?: yes, no (y/n")
+        if car_accident == "y":
+        print("high")
+        else:
+            print("madium")
